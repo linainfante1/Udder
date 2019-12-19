785c810dc9e8097f5bdf071cd73824c5c9f15c14
Lina Infante <linainfante14@gmail.com>
2019-12-18 23:14:22 -0800

    Commit original 4 files

---
+++ Udder/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by cate on 12/18/19.
+//  Copyright © 2019 cate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+         Text("Udder")
+            .fontWeight(.semibold)
+            .foregroundColor(Color.gray)
+        Text("Fresh Milk, On Demand")
+            .fontWeight(.semibold)
+            .foregroundColor(Color.gray)
+            
+        }
+      
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
